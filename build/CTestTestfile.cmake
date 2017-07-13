02070015cc74708bfa56e0db8be2c2052f434a08
Praneeth Behara <vbehara@buffalo.edu>
2017-07-13 18:35:35 -0400

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/praneeth/wifi-mapping/src
+# Build directory: /home/praneeth/wifi-mapping/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(wifi_drones)
---
+++ build/wifi_drones/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/praneeth/wifi-mapping/src/wifi_drones
+# Build directory: /home/praneeth/wifi-mapping/build/wifi_drones
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
